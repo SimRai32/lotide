4241da43c992408bed05ab08bb41148347321600
Simran Kalirai <kaliraisimran@gmail.com>
2022-08-22 21:01:50 +0000

    assertEqual done

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let trueOrFalse = true;
+  if (actual.toString().length !== expected.toString().length) {
+    trueOrFalse = false;
+  }
+  for (let i = 0; i < expected.toString().length; i++) {
+    if (actual.toString()[i] !== expected.toString()[i]) {
+      trueOrFalse = false;
+    }
+  }
+  if (!trueOrFalse) {
+    return console.log("Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    return console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(11, 12);
+assertEqual("dog", "dog");
