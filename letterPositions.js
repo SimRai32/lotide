6da897bd1ffb30fe480e5fbbb3d3e1ca0ac22e1c
Simran Kalirai <kaliraisimran@gmail.com>
2022-08-24 21:35:45 +0000

    letterPositions.js done

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  let trueOrFalse = true;
+  if (actual === undefined || expected === undefined) {
+    if (actual !== expected) {
+      trueOrFalse = false;
+    }
+  } else {
+    if (actual.toString().length !== expected.toString().length) {
+      trueOrFalse = false;
+    }
+    for (let i = 0; i < expected.toString().length; i++) {
+      if (actual.toString()[i] !== expected.toString()[i]) {
+        trueOrFalse = false;
+      }
+    }
+  }
+  if (!trueOrFalse) {
+    return console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    return console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const letterPositions = function(string) {
+  let letterPosition = {};
+  for (let index = 0; index < string.length; index++) {
+    if(string[index] !== " "){
+      if (letterPosition[string[index]]) {
+        letterPosition[string[index]].push(index);
+      } else {
+        letterPosition[string[index]] = [index];
+      }
+    }
+  }
+  return letterPosition;
+};
+
+const test = letterPositions("lighthouse in the house");
+
+assertEqual(test["l"], [0]);
+assertEqual(test["i"], [1,11]);
