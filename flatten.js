46075f5afb84f25a791ec9235a7f22f1034e1451
Simran Kalirai <kaliraisimran@gmail.com>
2022-08-24 02:00:33 +0000

    flatten.js done

---
+++ flatten.js
@@ -0,0 +1,18 @@
+const flatten = function(array) {
+  let flatArray = [];
+  let index = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      array[i].forEach(element => {
+        flatArray[index] = element;
+        index++;
+      });
+    } else {
+      flatArray[index] = array[i];
+      index++;
+    }
+  }
+  return console.log(flatArray);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]);
