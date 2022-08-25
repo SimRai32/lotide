76d80b2ea1deb8f50a449e3340caee42a2094e86
Simran Kalirai <kaliraisimran@gmail.com>
2022-08-25 21:39:25 +0000

    Made some adjustments to assertArraysEqual + finished map.js

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
+
+
+const map = (array, callback) => {
+  let arr = [];
+  for (const element of array) {
+    arr.push(callback(element));
+  }
+  return arr;
+}
+
+
 const eqArrays = function(arrayOne, arrayTwo) {
   let check = true;
   if (arrayOne.toString().length !== arrayTwo.toString().length) {
@@ -24,4 +35,14 @@ const checker = function(arrayOne, arrayTwo, result) {
   }
 };
 
+const words = ["ground", "control", "to", "major", "tom"];
+const nums =[1,2,3,4,5];
+const empty = [];
+const results1 = map(words, word => word[0]);
+const results2 = map(nums, num => num * 3);
+const results3 = map(empty, emptied => emptied * 0);
+console.log(results2);
+eqArrays(results1[0], "g");
+eqArrays(results2[3], 11);
+eqArrays(results3, []);
 
